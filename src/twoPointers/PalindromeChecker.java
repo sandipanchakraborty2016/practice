8e93c83b39aca7d3990024c8e121f3f11bc330aa
Sandipan <sandipanchakraborty@live.in>
2025-06-17 15:17:33 +0530

    Coding

---
+++ src/twoPointers/PalindromeChecker.java
@@ -0,0 +1,23 @@
+package twoPointers;
+
+public class PalindromeChecker {
+
+    public static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            char startChar = Character.toLowerCase(str.charAt(start));
+            char endChar=Character.toLowerCase(str.charAt(end));
+            if(startChar!=endChar){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isPalindrome("Madam"));
+    }
+}
