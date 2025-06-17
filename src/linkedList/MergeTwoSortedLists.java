330bfb44b39e8fc56e60addcecf41eab86e02b14
Sandipan <sandipanchakraborty@live.in>
2025-06-17 17:01:43 +0530

    Coding

---
+++ src/linkedList/MergeTwoSortedLists.java
@@ -0,0 +1,22 @@
+package linkedList;
+
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr=curr.next;
+        }
+        curr.next=l1==null?l2:l1;
+        return dummy.next;
+    }
+}
