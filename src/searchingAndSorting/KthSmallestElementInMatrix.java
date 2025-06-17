ac311a55ffcfa1c68b08f3e4080532a284246402
Sandipan <sandipanchakraborty@live.in>
2025-06-17 19:46:50 +0530

    Coding

---
+++ src/searchingAndSorting/KthSmallestElementInMatrix.java
@@ -0,0 +1,29 @@
+package searchingAndSorting;
+
+public class KthSmallestElementInMatrix {
+
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+            int j = n - 1;
+
+            for (int i = 0; i < n; i++) {
+                while (j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                count += j + 1;
+            }
+            if(count<k){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+}
