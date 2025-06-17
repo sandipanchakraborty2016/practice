7c48fde55296de8d907f1e0cbd80ddd39ad516a5
Sandipan <sandipanchakraborty@live.in>
2025-06-17 16:38:29 +0530

    Coding

---
+++ src/twoPointers/DetectCycle.java
@@ -0,0 +1,18 @@
+package twoPointers;
+
+public class DetectCycle {
+
+    public boolean isCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
